9cf498956218cbcc69918def169e451023d4b90f
Igor Graziano <igorrsgraziano@gmail.com>
2024-06-25 16:21:12 -0300

    created module to fix issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { AuthModule } from './user/auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, UserController],
+  imports: [AuthModule, UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
